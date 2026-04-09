780f7be39224afdeac307ef088ed4608d6da1260
Gwen-83 <gwenael.drouet@gmail.com>
2026-04-09 16:40:01 +0200

    error

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import home, devenir_benevole
+from core.views import home, devenir_benevole, contact, mentions_legales
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('devenir-benevole/', devenir_benevole, name='devenir_benevole'),
+    path('contact/', contact, name='contact'),
+    path('mention-legales/', mentions_legales, name='mentions légales')
 ]
